a75c7de9cc29ff66b66c8a683d5dca7390d78f06
Oleksandr <targonsky97@gmail.com>
2018-11-27 14:23:09 +0200

    refactor: use arrow functions

--- src/App.js
+++ src/App.js
@@ -8,18 +8,16 @@ class App extends Component {
     return (
       <div className="App">
         {
-          LIST.map( function (item) {
-            return (
-              <div key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </div>
-            );
-          })
+          LIST.map( item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          )
         }
       </div>
     );

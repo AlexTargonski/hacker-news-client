43e5e04d3c44a0b25f6709c7acb6d8982e7d3bb7
Oleksandr <targonsky97@gmail.com>
2018-11-28 11:57:52 +0200

    feat: add search

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import { LIST }             from './list';
 import './App.css';
 
+function isSearched(searchTerm) {
+  return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: LIST
+      list: LIST,
+      searchTerm: '',
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
@@ -18,11 +26,21 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
         {
-          this.state.list.map( item =>
+          this.state.list.filter(isSearched(this.state.searchTerm)).map( item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>

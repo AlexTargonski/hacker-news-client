bf42584b580cb2d9f096cbce00b6fda83f06d216
Oleksandr <targonsky97@gmail.com>
2018-11-29 12:42:01 +0200

    make input controlled

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
         <form>
           <input
             type="text"
+            value={searchTerm}
             onChange={this.onSearchChange}
           />
         </form>
